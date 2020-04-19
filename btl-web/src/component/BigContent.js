dd05dd5ed6c60d73424f52873ccf6893271384fb
phuongtrungduc <phgtrungduc@gmail.com>
2020-04-19 15:47:38 +0700

    thieu slide

--- btl-web/src/component/BigContent.js
+++ btl-web/src/component/BigContent.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import "../CSS/navbar.css";
 import { FaComments } from "react-icons/fa";
-import SlideShow from './SlideShow';
 class BigContent extends Component {
     render() {
         return (
             <div className="content">
                 <div className="big">
                     <div className="slide">
-                        
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eveniet accusantium sit neque facilis ratione officiis ipsa distinctio corrupti laborum ipsam, vitae obcaecati porro recusandae id soluta quaerat necessitatibus numquam.
+                        <h1>Chỗ này là slide, bổ sung sau</h1>
                     </div>
                     <div className="listevent">
                         <div className="title"><a href>Danh sách sự kiện</a></div>
@@ -23,43 +23,231 @@ class BigContent extends Component {
                                 <li><a href="#">Nhiếp ảnh, Phim, Video</a></li>
                             </ul>
                         </div>
-                        <div className="main">
+                        <div className="mainrow">
                             <div className="card">
                                 <a href="#">
-                                    <img src="images/card1.jpg" alt="" />
+                                    <img src="images/images1.jpg" alt="abc" />
                                     <h3>Triển lãm nghệ thuật cửng dái</h3>
                                 </a>
                                 <div className="about">
                                     <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
-                                    <FaComments />
+                                    <FaComments/>
                                     <div className="count">0</div>
                                 </div>
                                 <p>
                                     02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
-                                </p>
+                </p>
                             </div>
-                            <div className="card">
+                            <div className="card card2">
                                 <a href="#">
-                                    <img src="images/card2.jpg" alt="" />
+                                    <img src="./images/images2.jpg" alt="" />
                                     <h3>Triển lãm nghệ thuật cửng dái</h3>
                                 </a>
                                 <div className="about">
                                     <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
-                                    <FaComments />
+                                    <FaComments/>
                                     <div className="count">0</div>
                                 </div>
                                 <p>
                                     02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
-                    </p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis harum inventore eos unde dicta minima rem rerum quo maxime qui! Sint magnam, quisquam ea dolorem obcaecati doloremque maiores asperiores mollitia?
+                </p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="card1">
+                                <img src="images/images1.jpg" alt="" />
+                                <div className="about">
+                                    <a href><h4>Nghệ thuật trong thời kì Corona</h4></a>
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                </div>
+                            </div>
+                            <div className="card1 card2">
+                                <img src="images/images1.jpg" alt="" />
+                                <div className="about">
+                                    <a href><h4>Nghệ thuật trong thời kì Corona</h4></a>
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="card1">
+                                <img src="images/images1.jpg" alt="" />
+                                <div className="about">
+                                    <a href><h4>Nghệ thuật trong thời kì Corona</h4></a>
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                </div>
+                            </div>
+                            <div className="card1 card2">
+                                <img src="images/images1.jpg" alt="" />
+                                <div className="about">
+                                    <a href><h4>Nghệ thuật trong thời kì Corona</h4></a>
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="card1">
+                                <img src="images/images1.jpg" alt="" />
+                                <div className="about">
+                                    <a href><h4>Nghệ thuật trong thời kì Corona</h4></a>
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                </div>
+                            </div>
+                            <div className="card1 card2">
+                                <img src="images/images1.jpg" alt="" />
+                                <div className="about">
+                                    <a href><h4>Nghệ thuật trong thời kì Corona</h4></a>
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="card1">
+                                <img src="images/images1.jpg" alt="" />
+                                <div className="about">
+                                    <a href><h4>Nghệ thuật trong thời kì Corona</h4></a>
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                </div>
+                            </div>
+                            <div className="card1 card2">
+                                <img src="images/images1.jpg" alt="" />
+                                <div className="about">
+                                    <a href><h4>Nghệ thuật trong thời kì Corona</h4></a>
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="news">
                         <div className="title"><a href>Tin tức mới nhất</a></div>
                         <hr />
+                        <div className="row">
+                            <div className="card">
+                                <a href="#">
+                                    <img src="images/images1.jpg" alt="abc" />
+                                    <h3>Triển lãm nghệ thuật cửng dái</h3>
+                                </a>
+                                <div className="about">
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                    <FaComments/>
+                                    <div className="count">0</div>
+                                </div>
+                                <p>
+                                    02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
+                </p>
+                            </div>
+                            <div className="card card2">
+                                <a href="#">
+                                    <img src="images/images2.jpg" alt="" />
+                                    <h3>Triển lãm nghệ thuật cửng dái</h3>
+                                </a>
+                                <div className="about">
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                    <FaComments/>
+                                    <div className="count">0</div>
+                                </div>
+                                <p>
+                                    02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
+                </p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="card">
+                                <a href="#">
+                                    <img src="images/images1.jpg" alt="abc" />
+                                    <h3>Triển lãm nghệ thuật cửng dái</h3>
+                                </a>
+                                <div className="about">
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                    <FaComments/>
+                                    <div className="count">0</div>
+                                </div>
+                                <p>
+                                    02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
+                </p>
+                            </div>
+                            <div className="card card2">
+                                <a href="#">
+                                    <img src="images/images2.jpg" alt="" />
+                                    <h3>Triển lãm nghệ thuật cửng dái</h3>
+                                </a>
+                                <div className="about">
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                    <FaComments/>
+                                    <div className="count">0</div>
+                                </div>
+                                <p>
+                                    02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
+                </p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="card">
+                                <a href="#">
+                                    <img src="images/images1.jpg" alt="abc" />
+                                    <h3>Triển lãm nghệ thuật cửng dái</h3>
+                                </a>
+                                <div className="about">
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                    <FaComments/>
+                                    <div className="count">0</div>
+                                </div>
+                                <p>
+                                    02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
+                </p>
+                            </div>
+                            <div className="card card2">
+                                <a href="#">
+                                    <img src="images/images2.jpg" alt="" />
+                                    <h3>Triển lãm nghệ thuật cửng dái</h3>
+                                </a>
+                                <div className="about">
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                    <FaComments/>
+                                    <div className="count">0</div>
+                                </div>
+                                <p>
+                                    02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
+                </p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="card">
+                                <a href="#">
+                                    <img src="images/images1.jpg" alt="abc" />
+                                    <h3>Triển lãm nghệ thuật cửng dái</h3>
+                                </a>
+                                <div className="about">
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                    <FaComments/>
+                                    <div className="count">0</div>
+                                </div>
+                                <p>
+                                    02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
+                </p>
+                            </div>
+                            <div className="card card2">
+                                <a href="#">
+                                    <img src="images/images2.jpg" alt="" />
+                                    <h3>Triển lãm nghệ thuật cửng dái</h3>
+                                </a>
+                                <div className="about">
+                                    <div className="date" style={{ fontStyle: 'italic' }}>03/04/2020</div>
+                                    <FaComments/>
+                                    <div className="count">0</div>
+                                </div>
+                                <p>
+                                    02/04 - 30/04 - Triển lãm nghệ thuật nhằm nâng cao nhận thức cộng đồng về hội chứng tự kỷ
+                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="small" />
+                <div className="small">
+                    <div className="title"><a href>Có gì hôm nay</a></div>
+                    <hr />
+                </div>
             </div>
         );
     }
