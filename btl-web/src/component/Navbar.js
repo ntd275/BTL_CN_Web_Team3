d58702e2dba812e1b8ad2736f1b0b9820391a9ee
phuongtrungduc <phgtrungduc@gmail.com>
2020-04-19 09:18:55 +0700

    third commit

--- btl-web/src/component/Navbar.js
+++ btl-web/src/component/Navbar.js
@@ -3,21 +3,24 @@ import "../CSS/navbar.css";
 class Navbar extends Component {
     render() {
         return (
-            <div className="navbar">
-                <div className="left">
-                    <span><a href="#">Event</a></span>
-                </div>
-                <div className="right">
-                    <ul>
-                        <li><a href="#">Trang chủ</a></li>
-                        <li><a href="#">Lịch</a></li>
-                        <li><a href="#">Chuyên mục</a></li>
-                        <li><a href="#">Tin tức</a></li>
-                        <li><a href="#">Đối tác</a></li>
-                        <li><a href="#">Liên hệ</a></li>
-                        <li><a href="#">Tìm kiếm</a></li>
-                    </ul>
-                </div>
+            <div className="header">
+                <h2 className="logo">Group 3</h2>
+                <input type="checkbox" name id="chk" />
+                <label htmlFor="chk" className="show-menu-btn">
+                    <i className="fas fa-ellipsis-h" />
+                </label>
+                <ul className="menu">
+                    <a href="#">Trang chủ</a>
+                    <a href="#">Lịch</a>
+                    <a href="#">Chuyên mục</a>
+                    <a href="#">Tin tức</a>
+                    <a href="#">Đối tác</a>
+                    <a href="#">Liên hệ</a>
+                    <a href="#">Tìm kiếm</a>
+                    <label htmlFor="chk" className="hide-menu-btn">
+                        <i className="fas fa-times" />
+                    </label>
+                </ul>
             </div>
         );
     }
