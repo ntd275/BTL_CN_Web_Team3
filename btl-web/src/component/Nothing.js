0891187f5ac0fec9382c73f69f29ca3ddd43e1d8
phuongtrungduc <phgtrungduc@gmail.com>
2020-04-25 00:46:27 +0700

    full home page + router

--- btl-web/src/component/Nothing.js
+++ btl-web/src/component/Nothing.js
@@ -4,7 +4,7 @@ class Nothing extends Component {
     render() {
         return (
             <div>
-                <h1 style={{"margin-top":'200px',color:"black"}}>Nothing here</h1>
+                <h1 style={{"margin-top":'200px',color:"black",height:"100vh"}}>Nothing here</h1>
             </div>
         );
     }
