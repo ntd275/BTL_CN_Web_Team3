f9b0ef70721787b60b256202be2b56c96f5bc970
ntd275 <ntd275@gmail.com>
2020-05-13 16:29:58 +0700

    add cors

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,14 @@ let express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser'),
+    cors = require('cors'),
     path = require('path');
     
 require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors);
 app.use('/uploads',express.static(path.join(__dirname, 'uploads')));
 
 let routes = require('./api/routes/routes');
