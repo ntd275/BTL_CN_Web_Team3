f110b625691f384c29a96badb7ce10a72321c9e0
ducpb <kbs07101999@gmail.com>
2020-05-15 22:36:58 +0700

    ?

--- Backend/api/models/eventModel.js
+++ Backend/api/models/eventModel.js
@@ -1,37 +1,55 @@
 'use strict';
 let mongoose = require('../db');
 let mongoosePaginate = require('mongoose-paginate');
+let autoIncrement = require('mongoose-auto-increment');
+
 let Schema = mongoose.Schema;
 
 let EventSchema = new Schema({
   id: {
+    type: Number, //Kiểu dữ liệu
+    required: true, //Bắt buộc có
+    unique: true, // Các id phải khác nhau
+    index: true // Được đánh chỉ mục
+  },
+  user_create: {
     type: String,
     required: true,
-    unique: true,
-    index: true
   },
-  name: {
+  title: {
     type: String,
     required: true
   },
-  type: {
-      type: String,
+  image: {
+    type: String,
   },
-  date: {
-      type: Date,
+  category: {
+    type: String,
+  },
+  start_time: {
+    type: Date,
+  },
+  finish_time: {
+    type: Date,
+  },
+  locate:{
+    type: String,
+  },
+  address:{
+    type: String,
   },
   content: {
-      type: String,
-  },
-  comment: {
-      type: [],
+    type: [],
   },
   Created_date: {
     type: Date,
-    default: Date.now
+    default: Date.now // Ngày tạo mặc định
   },
 });
 
+//Thêm plugin phân trang
 EventSchema.plugin(mongoosePaginate);
+//Thêm plugin tự động tăng id
+EventSchema.plugin(autoIncrement.plugin, { model: 'Event', field: 'id' });
 
 module.exports = mongoose.model('Event', EventSchema);
