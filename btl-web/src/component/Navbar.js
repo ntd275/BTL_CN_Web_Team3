c67b1b6667861a2cd89fd913c0da6d8e33770230
phuongtrungduc <phgtrungduc@gmail.com>
2020-05-19 01:33:05 +0700

    fix navbar

--- btl-web/src/component/Navbar.js
+++ btl-web/src/component/Navbar.js
@@ -3,39 +3,12 @@ import "../CSS/navbar.css";
 import { FaTimes, FaEllipsisH } from "react-icons/fa";
 import { Link } from "react-router-dom";
 class Navbar extends Component {
-<<<<<<< HEAD
-    render() {
-        return (
-            <div className="header">
-                <h2 className="logo">Group 3</h2>
-                <input type="checkbox" name id="chk" />
-                <label htmlFor="chk" className="show-menu-btn">
-                    <FaEllipsisH />
-                </label>
-                <ul className="menu">
-                    <Link to="/" exact>
-                        <a>Trang chủ</a>
-                    </Link>
-                    <Link to="/calendar" exact>
-                        <a href="#">Lịch</a>
-                    </Link>
-                    <Link to="/categories" exact>
-                        <a href="#">Chuyên mục</a>
-                    </Link>
-                    <Link to="/news" exact>
-                        <a href="#">Tin tức</a>
-                    </Link>
-                    <Link to="/partners" exact>
-                        <a href="#">Đối tác</a>
-                    </Link>
-=======
   constructor(props) {
     super(props);
     this.state = {
       flagLink: 1,
     };
   }
->>>>>>> cbe71b8ac69e0a4a32e0b644a83ddc5b9cf9e0df
 
   hoverNavbar(id) {
     this.setState({
@@ -90,7 +63,7 @@ class Navbar extends Component {
             onClick={() => {
               this.hoverNavbar(4);
             }}
-            className={(flagLink == 4) ? "category-link" : "" }
+            className={(flagLink == 4) ? "category-link" : ""}
           >
             Tin tức
           </Link>
