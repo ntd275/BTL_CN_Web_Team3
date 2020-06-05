c09203ffec5b12a9a22ec5efbecc22c330ce2312
ntd275 <ntd275@gmail.com>
2020-06-05 15:45:48 +0700

    fix db

--- Backend/api/db.js
+++ Backend/api/db.js
@@ -4,11 +4,16 @@ let autoIncrement = require('mongoose-auto-increment');
 let user = process.env.USERDB;
 let password = process.env.PASSWORD;
 
-//Kết nối DB
+//connection string;
 let mongoDB = "mongodb://" + user + ":" + password + "@20.44.200.142:27017/WEB"
 
 //console.log(mongoDB);
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+//Kết nối DB
 mongoose.connect(mongoDB);
 //Kiểm tra kết nối
 let db = mongoose.connection;
